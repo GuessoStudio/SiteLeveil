adbbcbee8452e97ac40efc918da22fb8dfba35e7
GuessoStudio <soso191190@gmail.com>
2025-08-25 16:52:23 +0200

    ✨ Ajout logo moyen personnel dans le footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
               <img 
                 src="/public/images/logo-footer.jpg" 
                 alt="L'Éveil" 
-                className="w-10 h-10 rounded-full"
+                className=  "w-10 h-10 rounded-full"
               />
               <span className="text-xl font-bold text-white">L'Éveil</span>
             </div>
